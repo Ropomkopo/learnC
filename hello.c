b7ef9e70054f5e494eab41e68d5ba774bd138dc4
Roman Lysenko <romko.lisenko@gmail.com>
2025-12-29 21:06:07 +0200

    Added another type data.

--- hello.c
+++ hello.c
@@ -1,6 +1,11 @@
 #include <stdio.h>
+int age = 25;
+float height = 1.75;
+char letter = 'A';
 
-int main() {
-    printf("Hello, C!\n");
-    return 0;
+int main()
+{
+    printf("Age: %d\n", age);
+    printf("Height: %.2f\n", height);
+    printf("Letter: %c\n", letter);
 }
