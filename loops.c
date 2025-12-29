2c4c2839661981ef0af53cf1baed3cf90b7c6c86
Roman Lysenko <romko.lisenko@gmail.com>
2025-12-29 21:40:09 +0200

    Added loops for project.

---
+++ loops.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+    // int num = 1;
+
+    // while (num <= 100)
+    // {
+    //     num++;
+    //     if (num % 7 == 0)
+    //     {
+    //         printf("%d\n", num);
+    //         continue;
+    //     }
+    // }
+
+    for (int i = 0; i < 10; i++)
+    {
+        printf("%d\n", i);
+    }
+
+    return 0;
+}
